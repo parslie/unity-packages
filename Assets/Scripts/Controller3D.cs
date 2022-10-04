322782a7402c3c8bf76580eefc7d2e94ec5da360
Viktor Holta <viktor.holta@outlook.com>
2022-10-04 14:34:45 +0200

    Finish basic controller 3D & player script

--- Assets/Scripts/Controller3D.cs
+++ Assets/Scripts/Controller3D.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(CapsuleCollider))]
 public class Controller3D : MonoBehaviour
 {
-    const float SKIN_WIDTH = 0.01f;
+    const float SKIN_WIDTH = 0.02f;
 
+    [SerializeField]
+    LayerMask collisionMask;
     new CapsuleCollider collider;
+
+    public bool IsGrounded { get; private set; }
+    public bool IsCeilinged { get; private set; }
 
     void Awake()
     {
@@ -16,23 +21,58 @@ public class Controller3D : MonoBehaviour
 
     public void Move(Vector3 velocity)
     {
+        IsGrounded = false;
+        IsCeilinged = false;
+
         // Calculate collisions
         if (velocity.y != 0)
             VerticalCasting(ref velocity);
         if (velocity.x != 0 || velocity.z != 0)
             RadialCasting(ref velocity);
 
-
+        // Handle collisions
+        transform.Translate(velocity, Space.World);
     }
 
     void VerticalCasting(ref Vector3 velocity)
     {
+        Vector3 point1 = transform.position + collider.center + Vector3.down * (collider.height / 2 - collider.radius);
+        Vector3 point2 = transform.position + collider.center + Vector3.up * (collider.height / 2 - collider.radius);
+        
+        Vector3 direction = Vector3.up * Mathf.Sign(velocity.y);
+        float distance = Mathf.Abs(velocity.y) + SKIN_WIDTH;
 
+        RaycastHit hit;
+        if (Physics.CapsuleCast(point1, point2, collider.radius - SKIN_WIDTH, direction, out hit, distance, collisionMask))
+        {
+            Vector3 displacement = direction * (hit.distance - SKIN_WIDTH);
+            velocity.y = displacement.y;
+            // TODO: add collisionListener
+
+            IsGrounded = direction == Vector3.down;
+            IsCeilinged = direction == Vector3.up;
+        }
     }
 
     void RadialCasting(ref Vector3 velocity)
     {
-        
+        Vector3 point1 = transform.position + collider.center + Vector3.down * (collider.height / 2 - collider.radius);
+        Vector3 point2 = transform.position + collider.center + Vector3.up * (collider.height / 2 - collider.radius);
+
+        Vector3 direction = new Vector3(velocity.x, 0, velocity.z);
+        float distance = direction.magnitude + SKIN_WIDTH;
+        direction.Normalize();
+
+        RaycastHit hit;
+        if (Physics.CapsuleCast(point1, point2, collider.radius - SKIN_WIDTH, direction, out hit, distance, collisionMask))
+        {
+            // TODO: account for SKIN_WIDTH properly when going diagonally
+
+            Vector3 displacement = direction * (hit.distance - SKIN_WIDTH);
+            velocity.x = displacement.x;
+            velocity.z = displacement.z;
+            // TODO: add collisionListener
+        }
     }
 
     /*
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller3D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] float jogSpeed;
+    [SerializeField] float gravity;
+    Vector3 velocity;
+
+    Controller3D controller;
+
+    void Awake()
+    {
+        controller = GetComponent<Controller3D>();
+    }
+
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        velocity.x = jogSpeed * moveInput.x;
+        velocity.z = jogSpeed * moveInput.z;
+
+        if (controller.IsGrounded || controller.IsCeilinged)
+            velocity.y = 0;
+        velocity.y -= gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
